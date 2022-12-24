bee433ef827885890ecd12f25fc1bc019972bd24
IT'S KK <adeveloperkk@gmail.com>
2022-12-24 18:36:36 +0530

    Python

---
+++ two sum.py
@@ -0,0 +1,11 @@
+def twoSum(nums, target):
+        dic={}
+        for i in range(len(nums)):
+            dic[i]=nums[i]
+        print(dic)
+       #     for j in range(len(dic)):
+          #      print(dic[j]+)
+
+a=list(input(""))
+b=int(input(""))
+print(twoSum(a,b))
